6a50cacf23f45ae7a0f4929fdde9af9a00dfb6d2
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-02-08 11:02:59 +0000

    finished adding databases

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from rango import views
-from django.conf import from django.conf import settingsfrom django.conf.urls.static import static
+
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
 
 app_name='rango'
 urlpatterns=[
