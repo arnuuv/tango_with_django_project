dc6d31ad8ba870aa50070a391b8db85e9fd1cf74
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-02-08 14:21:43 +0000

    Password Hashing

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -85,6 +85,10 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -95,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {'min_length':6,}
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
